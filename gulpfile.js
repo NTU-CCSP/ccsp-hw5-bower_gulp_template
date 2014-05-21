353552c3b264719f4561f3e56ba57c3fcca5401d
tomchentw <developer@tomchentw.com>
2014-05-22 00:17:08 +0800

    feat(gulpfile.js): add gulp-uglify and create compress task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp    = require('gulp');
 var jshint  = require('gulp-jshint');
+var uglify = require('gulp-uglify');
 
 gulp.task('lint', function () {
   return gulp.src(['./public/javascripts/*.js', './gulpfile.js'])
   .pipe(jshint())
   .pipe(jshint.reporter('default'));
-})
+});
+
+gulp.task('compress', function () {
+  return gulp.src('./public/javascripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./tmp/javascripts'));
+});
