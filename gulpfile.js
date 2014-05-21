9ba78af449e5f749c9b4f754733c112fe9afdd59
tomchentw <developer@tomchentw.com>
2014-05-22 00:22:11 +0800

    feat(gulpfile.js): add gulp-concat plugin
    * rename compress to scripts task
    * remove semicolon in index.js to generate jshint error

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require('gulp');
 var jshint  = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 gulp.task('lint', function () {
   return gulp.src(['./public/javascripts/*.js', './gulpfile.js'])
@@ -8,8 +9,11 @@ gulp.task('lint', function () {
   .pipe(jshint.reporter('default'));
 });
 
-gulp.task('compress', function () {
+gulp.task('scripts', function () {
   return gulp.src('./public/javascripts/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('application.js'))
   .pipe(uglify())
   .pipe(gulp.dest('./tmp/javascripts'));
 });
